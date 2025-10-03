5c3159895f47d380bf1fcc9353b0eafd8dd66f22
Juan Pablo Rivera <141880338+SugarBrownie@users.noreply.github.com>
2025-10-02 22:28:03 -0500

    Inicializado

---
+++ .next/server/app/favicon.ico/route.js
@@ -0,0 +1,6 @@
+var R=require("../../chunks/[turbopack]_runtime.js")("server/app/favicon.ico/route.js")
+R.c("server/chunks/5b5de_next_ca6021aa._.js")
+R.c("server/chunks/[root-of-the-server]__7ed754c5._.js")
+R.m("[project]/Desktop/WEB/ProyectoWeb/.next-internal/server/app/favicon.ico/route/actions.js [app-rsc] (server actions loader, ecmascript)")
+R.m("[project]/Desktop/WEB/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/Desktop/WEB/ProyectoWeb/src/app/favicon--route-entry.js [app-route] (ecmascript)\" } [app-route] (ecmascript)")
+module.exports=R.m("[project]/Desktop/WEB/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/Desktop/WEB/ProyectoWeb/src/app/favicon--route-entry.js [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"05vfLdHnp7Ke+n6aOB+Qaqf4K5ReIJrh8jbialgglCI=\"\n}"
