805e19098d370b782073221f6e01da5ec6c54d68
Juan Pablo Rivera <141880338+SugarBrownie@users.noreply.github.com>
2025-10-05 10:46:25 -0500

    i18n

--- .next/static/chunks/Desktop_WEB_ProyectoWeb_src_app_layout_tsx_341317e7._.js
+++ .next/static/chunks/Desktop_WEB_ProyectoWeb_src_app_layout_tsx_341317e7._.js
@@ -1,7 +1,9 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/[root-of-the-server]__ebb4e3e5._.css"
+  "static/chunks/[root-of-the-server]__ebb4e3e5._.css",
+  "static/chunks/5b5de_1322cada._.js",
+  "static/chunks/Desktop_WEB_ProyectoWeb_src_app_87f8d44b._.js"
 ],
     source: "dynamic"
 });
